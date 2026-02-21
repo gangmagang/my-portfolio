c755b118020c2612167bcdb127fbf12237c3305a
gangmangang <bonho96@naver.com>
2026-02-21 22:10:44 +0900

    fix: type DefaultTheme from theme object

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -1,5 +1,8 @@
+// src/styles/styled.d.ts
 import "styled-components";
-import type { AppTheme } from "./theme";
+import type { theme } from "./theme";
+
+type AppTheme = (typeof theme)[keyof typeof theme];
 
 declare module "styled-components" {
   export interface DefaultTheme extends AppTheme {}
